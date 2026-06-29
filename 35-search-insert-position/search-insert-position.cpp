9dfc5696b3d774f7e6533b7d98241b888d7b2f1f
Abhinav Asthana <143867960+asthana-16@users.noreply.github.com>
2026-06-29 11:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (77.43%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for(int i=0;i <nums.size();i++){
-            if(nums[i] >= target){
-                return i;
-            }
+        //basic linear approch
+        // for(int i=0;i <nums.size();i++){
+            // if(nums[i] >= target){
+                // return i;
+            // }
+        // }
+        // return nums.size();
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-        return nums.size();
+        return low;
     }
 };
