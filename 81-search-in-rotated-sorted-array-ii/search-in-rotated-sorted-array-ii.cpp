24f275d99db27ac4540e8536660779d68198f3c4
Abhinav Asthana <143867960+asthana-16@users.noreply.github.com>
2026-06-29 10:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.16%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -3,8 +3,39 @@ public:
     bool search(vector<int>& nums, int target) {
         //basic approach
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target) return true;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target) return true;
+        // }
+        // return false;
+        //Binary Search
+        int start = 0; 
+        int end = n-1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[start]==nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+                continue;
+            }
+            else if(nums[start] <= nums[mid]){
+                if (target >= nums[start] &&
+                    target < nums[mid])
+
+                    end = mid - 1;
+
+                else
+                    start = mid + 1;
+            }
+            else{
+                if (target > nums[mid] &&
+                    target <= nums[end])
+
+                    start = mid + 1;
+
+                else
+                    end = mid - 1;
+            }
         }
         return false;
     }
