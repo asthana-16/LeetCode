db1c960df613d5eacd46cd520740978c6354710a
Abhinav Asthana <143867960+asthana-16@users.noreply.github.com>
2026-07-13 09:45:37 +0530

    Time: 56 ms (60.37%) | Memory: 24 MB (26.40%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,17 +9,24 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        set<ListNode*> nodes;
-        while(headA){
-            nodes.insert(headA);
-            headA= headA->next;
+        // set<ListNode*> nodes;
+        // while(headA){
+        //     nodes.insert(headA);
+        //     headA= headA->next;
+        // }
+        // while(headB){
+        //     if(nodes.count(headB)){
+        //         return headB;
+        //     }
+        //     headB=headB->next;
+        // }
+        // return NULL;
+        //Optimal Approach 
+        ListNode* curr1= headA, *curr2=headB;
+        while(curr1 != curr2){
+            curr1= (curr1 == NULL) ? headB: curr1->next;
+            curr2= (curr2 == NULL) ? headA: curr2->next;
         }
-        while(headB){
-            if(nodes.count(headB)){
-                return headB;
-            }
-            headB=headB->next;
-        }
-        return NULL;
+        return curr1;
     }
 };
